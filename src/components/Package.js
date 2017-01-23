325e5ffc155be06a5b3e17681870cabc2ed0b92c
Fleur Kelpin <f.kelpin@umcg.nl>
2017-01-23 12:45:09 +0100

    Use pre-existing react bootstrap components

--- src/components/Package.js
+++ src/components/Package.js
@@ -1,29 +1,39 @@
 import React, {PropTypes} from "react";
+import {Glyphicon} from "react-bootstrap";
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
 
-const Package = ({entityTypes, packages, handleSelectPackage, handleSelectEntityType}) => (
-    <table className="table">
-        <thead>
-        <tr>
-            <th></th>
-            <th>name</th>
-            <th>description</th>
-        </tr>
-        </thead>
-        <tbody>
-        {packages.map((pack, i) =>
-            <tr key={"P" + i} onClick={() => handleSelectPackage(pack.fullName)}>
-                <td><span className="glyphicon glyphicon-folder-open" aria-hidden="true"></span></td>
-                <td>{pack.label}</td>
-                <td>{pack.description}</td>
-            </tr>)}
-        {entityTypes.map((entityType, i) =>
-            <tr key={"E" + i} onClick={() => handleSelectEntityType(entityType.fullName)}>
-                <td><span className="glyphicon glyphicon-list" aria-hidden="true"></span></td>
-                <td>{entityType.label}</td>
-                <td>{entityType.description}</td>
-            </tr>)}
-        </tbody>
-    </table>)
+const Package = ({entityTypes, packages, handleSelectPackage, handleSelectEntityType}) => {
+    const addType = type => item => ({
+        type,
+        ...item
+    })
+    const glyphs = {'entity': 'list', 'package': 'folder-open'}
+    const nameFormatter = (cell, row) => {
+        return <span><Glyphicon glyph={glyphs[row.type]}/>&nbsp;&nbsp;{row.label}</span>
+    }
+    const data = [...packages.map(addType('package')), ...entityTypes.map(addType('entity'))];
+    // const options = {onRowClick: onSelect}
+    const options = {
+        onRowClick: function (row) {
+            switch (row.type) {
+                case 'entity':
+                    handleSelectEntityType(row.fullName)
+                    break
+                case 'package':
+                    handleSelectPackage(row.fullName)
+                    break
+                default:
+            }
+        },
+        onRowDoubleClick: function (row) {
+            alert(`You double click row id: ${row.fullName}`);
+        }
+    };
+    return <BootstrapTable data={data} options={options}>
+        <TableHeaderColumn dataField='fullName' isKey={true} dataFormat={nameFormatter}>Name</TableHeaderColumn>
+        <TableHeaderColumn dataField="description">Description</TableHeaderColumn>
+    </BootstrapTable>
+}
 
 Package.propTypes = {
     entityTypes: PropTypes.array.isRequired,
--- src/components/PackagePath.js
+++ src/components/PackagePath.js
@@ -1,12 +1,19 @@
 import React, {PropTypes} from "react";
+import {Breadcrumb} from "react-bootstrap";
 
 const PackagePath = ({packagePath, handleSelectPackage}) =>
-    <ol className="breadcrumb">
-        {packagePath.map((p, i) => <li key={p.fullName}><a href="#" onClick={(e) => {
-            e.preventDefault();
-            handleSelectPackage(p.fullName)
-        }}>{p.label}</a></li>)}
-    </ol>
+    <Breadcrumb>
+        {packagePath.map((p, i) =>
+            <Breadcrumb.Item
+                key={p.fullName}
+                href="#"
+                onClick={(e) => {
+                    e.preventDefault();
+                    handleSelectPackage(p.fullName)
+                }}>
+                {p.label}
+            </Breadcrumb.Item>)}
+    </Breadcrumb>
 
 PackagePath.propTypes = {
     packagePath: PropTypes.array.isRequired,
